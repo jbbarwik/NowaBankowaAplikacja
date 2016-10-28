f904129963975c893bbbca6bdaf684b4244b3e4a
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-10-28 12:43:54 +0200

    stara metoda usunięta!!!! Jupi!!

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -3,11 +3,6 @@ import java.util.Scanner;
 
 public class Bank {
 
-    @Deprecated
-    public void zrobOperacje(Scanner inn) {
-        PrintStream komunikat = System.out;
-        zrobOperacje(inn, komunikat);
-    }
     public void zrobOperacje(Scanner inn, PrintStream komunikat) {
         Konto konto1 = new Konto("123456789", "Kowalski", 0);
         Konto konto2 = new Konto("123456766", "Kowalska", 0);
