51d494c511eec8eecc3ef463c6d1ace63bbefeea
sebian <informatyk@interia.com>
2016-10-28 11:00:20 +0200

    pkotest3

--- src/main/java/TestpkoSJ.java
+++ src/main/java/TestpkoSJ.java
@@ -8,7 +8,7 @@ public class TestpkoSJ {
         public static void main(String[] args) throws FileNotFoundException {
             File plikZOperacji = new File("test.txt");
             Scanner scanner2 = new Scanner(plikZOperacji);
-            new Bank().zrobOperacje(scanner2);
+            new Bank().zrobOperacje(scanner2); // No Comments
         }
     }
 
