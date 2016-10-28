401496937fcc39ad397bcc361728d60f331c23c9
Student <dsdsd@ovh.com>
2016-10-28 12:35:02 +0200

    ParaBankJB Kredyty, czyszczenie konta

--- src/main/java/TestPkoJB.java
+++ src/main/java/TestPkoJB.java
@@ -1,15 +1,25 @@
+import javax.servlet.ServletOutputStream;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
+import static spark.Spark.get;
+
 public class TestPkoJB {
     public static void main(String[] args) throws IOException {
         Path plik = Paths.get("C:\\Users\\Student\\Desktop\\Nowy folder\\NowaBankowaAplikacja\\src\\main\\java\\k.txt");
-        //File plik2 = new File("C:\\Users\\Student\\Desktop\\Nowy folder\\NowaBankowaAplikacja\\src\\main\\java\\k.txt");
-        Scanner skanerTxt = new Scanner(plik);
-        new Bank().zrobOperacje(skanerTxt);
+        File plik2 = new File("C:\\Users\\Student\\Desktop\\Nowy folder\\NowaBankowaAplikacja\\src\\main\\java\\k.txt");
+       Scanner skanerTxt = new Scanner(plik);
+        new Bank().zrobOperacje(skanerTxt, System.out); ///<----- w klasie Bank pojawiła się nowa metoda  do wykonywania operacji. Stara metoda została oznaczona @Deprecated stara metoda to skanerTxt, nowa metoda to system.out
+
+
+
+
+
+
     }
 }
