0b337b6a9521f8ff6bc529c04a0cdb7a5e675429
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-10-28 12:39:38 +0200

    poprawka starej metody

--- src/main/java/MainPko.java
+++ src/main/java/MainPko.java
@@ -5,7 +5,7 @@ public class MainPko {
     public static void main(String[] args) {
         Scanner inn = new Scanner(System.in);
         Bank pko = new Bank();
-        pko.zrobOperacje(inn);
+        pko.zrobOperacje(inn, System.out);
     }
 
 }
