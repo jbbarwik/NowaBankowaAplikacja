44c670ec07a093c902a78c008325d5ee86d9ee46
sebian <informatyk@interia.com>
2016-10-28 10:57:44 +0200

    pko

---
+++ src/main/java/TestpkoSJ.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TestpkoSJ {
+
+
+        public static void main(String[] args) throws FileNotFoundException {
+            File plikZOperacji = new File("test.txt");
+            Scanner scanner2 = new Scanner(plikZOperacji);
+            new Bank().zrobOperacje(scanner2);
+        }
+    }
+
+
