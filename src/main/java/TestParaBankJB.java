0c7670a3bfe7d5cecc167bd5dc7f4efb1072f528
Student <dsdsd@ovh.com>
2016-10-28 12:40:57 +0200

    11

--- src/main/java/TestParaBankJB.java
+++ src/main/java/TestParaBankJB.java
@@ -7,6 +7,6 @@ public class TestParaBankJB {
     public static void main(String[] args) throws FileNotFoundException {
         File plikZOperacjami = new File("C:\\Users\\Student\\Desktop\\Nowy folder\\NowaBankowaAplikacja\\src\\main\\java\\pk.txt");
         Scanner scanner2 = new Scanner(plikZOperacjami);
-        new Bank().zrobOperacje(scanner2);
+        new Bank().zrobOperacje(scanner2, System.out);
     }
 }
